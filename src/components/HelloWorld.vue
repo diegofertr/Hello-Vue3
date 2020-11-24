e9a3a278ebb29aaff896d19f21426dffb7272ad4
Diego F. Ticona Ramos <diegonano09@gmail.com>
2020-11-24 17:07:12 -0400

    Variables reactivas y composition api basicos

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,66 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <hr>
+    <div>
+      <h1>Contador</h1>
+      <h2 class="counter">{{ counter }}</h2>
+      <button class="btn-primary" @click="decrement">-</button>
+      <button class="btn-primary" @click="increment">+</button>
+    </div>
+    <hr>
+    <div>
+      <h1>Model</h1>
+      <label for="nombre">Nombre: </label>
+      <input name="nombre" class="txt-input" type="text" v-model="nombre" placeholder="Introduzca su nombre">
+      <p>
+        <strong>Su nombre es:</strong> {{ nombre }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import { onMounted, ref } from 'vue'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  setup () {
+    // Creando variables reactivas
+    const counter = ref( 0 )
+    const nombre = ref( '' )
+
+    // Funciones o métodos
+    const increment = () => {
+      counter.value++
+    }
+
+    const decrement = () => {
+      counter.value--
+    }
+
+    const setNombre = () => {
+      nombre.value = 'diegofer'
+    }
+    const setCounter = () => {
+      counter.value = 10
+    }
+
+    // Asignando datos de entrada al montar el componente
+    onMounted(setNombre)
+    onMounted(setCounter)
+
+
+    return {
+      // variables reactivas
+      counter,
+      nombre,
+      // funciones
+      increment,
+      decrement,
+    }
   }
 }
 </script>
@@ -54,5 +80,38 @@ li {
 }
 a {
   color: #42b983;
+  text-decoration: none;
 }
+.counter {
+  margin: 1rem 0;
+  font-size: 3rem;
+}
+.btn-primary {
+  padding: .5rem 2rem;
+  margin: 0 1rem;
+  border-radius: 10px;
+  border: none;
+  background: #091F92;
+  color: white;
+  font-size: 1.5rem;
+}
+.btn-primary:focus {
+  outline: none;
+}
+.btn-primary:hover {
+  opacity: .9;
+}
+.txt-input {
+  margin: 1rem 0;
+}
+hr {
+  margin: 2rem 0;
+}
+input {
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 1rem;
+  box-shadow: transparent;
+}
+
 </style>
